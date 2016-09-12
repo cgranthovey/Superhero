d7c6fee470563a83ebf5ea8ba19f36e510795be4
Chris Hovey <chrishovey@Chriss-MacBook-Air.local>
2016-09-12 15:04:24 -0600

    my first commit woot woot

---
+++ Superman/Superman.swift
@@ -0,0 +1,13 @@
+//
+//  Superman.swift
+//  Superman
+//
+//  Created by Chris Hovey on 9/12/16.
+//  Copyright © 2016 Chris Hovey. All rights reserved.
+//
+
+import Foundation
+
+class Superman {
+    var name: String!
+}
