19ece9c21f0d5d1bc893d4f291902b9a39ff0b0f
cgranthovey <cgranthovey@gmail.com>
2016-09-12 15:10:25 -0600

    Update ViewController.swift

--- Superman/ViewController.swift
+++ Superman/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         var myHero: Superman!
         
+        var x = 3
+        
     }
 
     override func didReceiveMemoryWarning() {
